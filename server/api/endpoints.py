91a5fc4f2f876c1b22f2bf8ade220737b3f8973b
4wqrs12 <forzstreet61@gmail.com>
2026-02-19 15:34:50 -0500

    stop tracking for pycache, updated gitignore, fixed login route for backend

--- server/api/endpoints.py
+++ server/api/endpoints.py
@@ -4,18 +4,20 @@ from api.config import user_col, bcrypt, datetime
 
 endpoints = Blueprint("endpoints", __name__)
 
-#decrypt passwords before checking login info
 @endpoints.route("/api/login", methods=["POST"])
 def login():
     data = request.get_json()
     username = data.get("username", "")
     password = data.get("password", "")
     if not username or not password:
-        return jsonify({"success": False, "message": "Enter both username and password", "data": [username, password]})
-    if not user_col.find_one({"username": username, "password": password}):
-        return jsonify({"success": False, "message": "Incorrect username or password", "data": [username, password]})
-    access_token = create_access_token(identity=username)
-    return jsonify({"success": True, "message": "Logged in", "data": {"accessToken": access_token}})
+        return jsonify({"success": False, "message": "Please enter both username and password", "data": data})
+    if not user_col.find_one({"username": username}):
+        return jsonify({"success": False, "message": "User does not exist", "data": data})
+    if bcrypt.check_password_hash(user_col.find_one({"username": username})["password"], password):
+        access_token = create_access_token(identity=username)
+        return jsonify({"success": True, "message": "Logged in", "data": {"accessToken": access_token}})
+    else:
+        return jsonify({"success": False, "message": "Incorrect password", "data": data})
 
 @endpoints.route("/api/register", methods=["POST"])
 def register():
@@ -23,9 +25,9 @@ def register():
     username = data.get("username", "")
     password = data.get("password", "")
     if not username or not password:
-        return jsonify({"success": False, "message": "Enter username and password", "data": [username, password]})
+        return jsonify({"success": False, "message": "Enter username and password", "data": data})
     if user_col.find_one({"username": username}):
-        return jsonify({"success": False, "message": "User already exists", "data": [username, password]})
+        return jsonify({"success": False, "message": "User already exists", "data": data})
     hashed_pass = bcrypt.generate_password_hash(password).decode("UTF-8")
     user_col.insert_one({"username": username, "password": hashed_pass, "createdAt": datetime.now()})
     access_token = create_access_token(identity=username)
