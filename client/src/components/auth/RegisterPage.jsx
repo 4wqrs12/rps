90e9f5b4e0b2b42cea542dcab0296e007eb93ad1
4wqrs12 <forzstreet61@gmail.com>
2026-02-23 11:12:30 -0500

    made login/register box seperate component

--- client/src/components/auth/RegisterPage.jsx
+++ client/src/components/auth/RegisterPage.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import UserAuth from "./UserAuth";
 
 function RegisterPage() {
   const [username, setUsername] = useState("");
@@ -32,39 +33,47 @@ function RegisterPage() {
   }
 
   return (
-    <div className="route-content">
-      <h1 className="route-title">Register</h1>
-      <div className="bg-[#C19A6B] w-64 rounded-md">
-        <form onSubmit={registerUser} className="p-5">
-          <div className="mb-5">
-            <input
-              type="text"
-              placeholder="Username..."
-              value={username}
-              onChange={handleUsernameChange}
-              className="input-field"
-            />
-          </div>
-          <div className="mb-5">
-            <input
-              type="text"
-              placeholder="Password..."
-              value={password}
-              onChange={handlePasswordChange}
-              className="input-field"
-            />
-          </div>
-          <div>
-            <button
-              type="submit"
-              className="btn bg-[#6E260E] p-3 hover:bg-[#571e0b]"
-            >
-              Register
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
+    // <div className="route-content">
+    //   <h1 className="route-title">Register</h1>
+    //   <div className="bg-[#C19A6B] w-64 rounded-md">
+    //     <form onSubmit={registerUser} className="p-5">
+    //       <div className="mb-5">
+    //         <input
+    //           type="text"
+    //           placeholder="Username..."
+    //           value={username}
+    //           onChange={handleUsernameChange}
+    //           className="input-field"
+    //         />
+    //       </div>
+    //       <div className="mb-5">
+    //         <input
+    //           type="text"
+    //           placeholder="Password..."
+    //           value={password}
+    //           onChange={handlePasswordChange}
+    //           className="input-field"
+    //         />
+    //       </div>
+    //       <div>
+    //         <button
+    //           type="submit"
+    //           className="btn bg-[#6E260E] p-3 hover:bg-[#571e0b]"
+    //         >
+    //           Register
+    //         </button>
+    //       </div>
+    //     </form>
+    //   </div>
+    // </div>
+    <UserAuth
+      title={"Register"}
+      user={username}
+      userHandler={handleUsernameChange}
+      pass={password}
+      passHandler={handlePasswordChange}
+      fetchFunc={registerUser}
+    />
   );
 }
 
---
+++ client/src/components/auth/UserAuth.jsx
@@ -0,0 +1,39 @@
+function UserAuth({ title, user, userHandler, pass, passHandler, fetchFunc }) {
+  return (
+    <div className="route-content">
+      <div className="route-title">{title}</div>
+      <div className="bg-[#C19A6B] w-64 rounded-md">
+        <form onSubmit={fetchFunc} className="p-5">
+          <div className="mb-5">
+            <input
+              type="text"
+              placeholder="Username..."
+              value={user}
+              onChange={userHandler}
+              className="input-field"
+            />
+          </div>
+          <div className="mb-5">
+            <input
+              type="text"
+              placeholder="Password"
+              value={pass}
+              onChange={passHandler}
+              className="input-field"
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="btn bg-[#6E260E] p-3 hover:bg-[#571E0B]"
+            >
+              {title}
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default UserAuth;
