a764258b49d3f63aee4dca77ccc356137d4e5bd6
4wqrs12 <forzstreet61@gmail.com>
2026-06-19 15:16:42 -0400

    test

--- client/src/utils/refreshToken.js
+++ client/src/utils/refreshToken.js
@@ -1,13 +1,15 @@
 export async function refreshToken() {
-  try {
-    const refreshRes = await fetch("http://localhost:5000/api/refresh", {
-      method: "POST",
-      credentials: "include",
-    });
-    const refreshData = await refreshRes.json();
-    return refreshData;
-  } catch (err) {
-    console.log("Refresh token API failed.");
-    return { success: false, message: "Refresh failed" };
-  }
+	try {
+		const refreshRes = await fetch("http://localhost:5000/api/refresh", {
+			method: "POST",
+			credentials: "include",
+		});
+		const refreshData = await refreshRes.json();
+		return refreshData;
+	} catch (err) {
+		console.log("Refresh token API failed.");
+		return {
+			success: false, message: "Refresh failed"
+		};
+	}
 }
